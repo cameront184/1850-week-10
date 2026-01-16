831cea422a83032f26754ab4872e09da77d030f8
cameront184 <dtxp0622@leeds.ac.uk>
2026-01-16 16:30:13 +0000

    H

--- week-10-session-1-main/ziplist.py
+++ week-10-session-1-main/ziplist.py
@@ -6,17 +6,14 @@
 
 import sys
 import zipfile
-from zipfile import ZipFile   # methods in this module can be used
+from zipfile import ZipFile, BadZipFile  # methods in this module can be used
 
 if len(sys.argv) < 2:
     print("Usage: python ziplist.py <file.zip>", file=sys.stderr)            # Gives an error message if user doesnt input a file
     sys.exit(1)
 
 archive_filename = sys.argv[1]
-
-if not zipfile.is_zipfile(archive_filename):                                 # error message if the file the user inputed is not a zip
-    print(f"Bad zip file: python ziplist.py {archive_filename}", file=sys.stderr)                         # all error mesages pass to the stderr channel if it fails
-    sys.exit(1)
+                       
 try:
     with ZipFile(archive_filename, "r") as infile:         # Used a zipfile.namelist to list the contents in the zip
         for name in infile.namelist():
@@ -24,4 +21,8 @@ try:
 except FileNotFoundError:
     print(f"File not found: python ziplist.py {archive_filename}", file=sys.stderr)         # error message for if the file that the user has inputed doesnt exist
     sys.exit(1)
+
+except BadZipFile:
+    print(f"Bad zip file: python ziplist.py {archive_filename}", file=sys.stderr)           # error message if the file the user inputed is not a zip
+    sys.exit(1)                                                                             # all error mesages pass to the stderr channel if it fails
 
